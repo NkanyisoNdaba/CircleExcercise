dc15d872df41ddb3d4cf97748e88b0d8bb561e13
nkanyiso <ndaba.nkanyi@live.co.uk>
2018-07-30 08:53:21 +0200

    circle program

---
+++ CircleExcercise/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleExcercise
+{
+    public class Circle
+    {
+        public int Radius { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Circle():this(1,2,2)
+        {
+           
+        }
+
+        public  Circle(int radius, int x, int y)
+        {
+            this.Radius = radius;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void GetLocation()
+        {
+
+            Console.WriteLine("Circle[centre=("+X+","+Y+") ,radius=" +Radius+"]");
+
+        }
+
+    }
+}
---
+++ CircleProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircleExcercise;
+
+namespace CircleProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle {};
+                circle.GetLocation();
+                Console.Read();
+        }
+    }
+
+}
